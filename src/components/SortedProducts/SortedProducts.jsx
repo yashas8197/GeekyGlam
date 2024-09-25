f1381e2d81611d59a203664aac9b0c5690f9e213
yashas8197 <yashasjr7@gmail.com>
2024-09-25 13:30:35 +0530

    style fix

--- src/components/SortedProducts/SortedProducts.jsx
+++ src/components/SortedProducts/SortedProducts.jsx
@@ -2,6 +2,7 @@ import ProductCard from "../ProductCard/ProductCard";
 
 const SortedProducts = ({ currentProduct, loading }) => {
   const placeholderCards = Array(6).fill(null);
+
   return (
     <div className="grid grid-cols-1 2xl:grid-cols-3 xl:grid-cols-3 m-4 md:grid-cols-2 lg:grid-cols-2 sm:grid-cols-3 gap-4">
       {loading === "loading" &&
@@ -10,8 +11,10 @@ const SortedProducts = ({ currentProduct, loading }) => {
             <div className="animate-shimmer bg-gray-200 sm:h-96 h-48 w-full rounded"></div>
           </div>
         ))}
-      {currentProduct.length === 0 && (
-        <p className="text-center text-xl">No products available.</p>
+      {loading !== "loading" && currentProduct.length === 0 && (
+        <div className="col-span-full flex justify-center items-center h-48">
+          <p className="text-center text-xl">No products available.</p>
+        </div>
       )}
       {loading !== "loading" &&
         currentProduct.map((product) => (
